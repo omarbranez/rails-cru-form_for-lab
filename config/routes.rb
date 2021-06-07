cba164afe6872721e2523416ff535a4522cd2265
Omar Branez <omar.branez@gmail.com>
2021-06-07 16:56:24 -0400

    commit tree 5221dc1505bf1ab9311c6ccec8e8f9449316c46a

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :songs, except: [:delete]
+  resources :artists, except: [:delete]
+  resources :genres, except: [:delete]
 end
